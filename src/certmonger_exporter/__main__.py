64234a2319fe59f5c65cf0b11c9fc5d19b6034d4
Sam Morris <sam@robots.org.uk>
2024-03-27 18:10:46 +0000

    Dump traceback of all threads on reception of SIGUSR1

--- src/certmonger_exporter/__main__.py
+++ src/certmonger_exporter/__main__.py
@@ -1,9 +1,11 @@
 import faulthandler
+import signal
 import sys
 
 from certmonger_exporter import * 
 
 faulthandler.enable()
+faulthandler.register(signal.SIGUSR1, all_threads=True)
 configure_logging()
 sys.excepthook = excepthook
 sys.exit(main(sys.argv))
